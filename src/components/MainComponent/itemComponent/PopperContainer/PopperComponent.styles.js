2d39c3275ba2bff0d34203d0976466562d57d6dc
Carlos Toro <carlostorom.96@gmail.com>
2023-04-29 14:33:50 -0400

    feat(common): set fix details and add carrousel lading page articles [WEBSITES-10]

--- src/components/MainComponent/itemComponent/PopperContainer/PopperComponent.styles.js
+++ src/components/MainComponent/itemComponent/PopperContainer/PopperComponent.styles.js
@@ -1,13 +1,47 @@
 import { makeStyles } from '@material-ui/core';
+import mq from '../../../../config/mq';
 
 const useStyles = makeStyles( theme => ({
   box: {
     display: 'flex', 
+    width: '1.8rem',
     flexDirection: 'column', 
     backgroundColor: theme.palette.secondary.main, 
     paddingTop: '0.5rem',
     paddingBottom: '1rem',
-    gap: 18,
+    gap: 10,
+    [mq('md')]: {
+      display: 'flex', 
+      width: '3rem',
+      flexDirection: 'column', 
+      backgroundColor: theme.palette.secondary.main, 
+      paddingTop: '0.5rem',
+      paddingBottom: '1rem',
+      gap: 18,
+    },
   },
+  icons: {
+    display: 'flex',
+    justifyContent: 'end',
+    alignItems: 'end',
+    paddingRight: '1rem',
+    gap: 10,
+    marginTop: '-1.5rem',
+    '& img': {
+      width: '15px',
+    },
+    [mq('md')]: {
+      display: 'flex',
+      justifyContent: 'end',
+      alignItems: 'end',
+      paddingRight: '1rem',
+      gap: 18,
+      marginTop: '0rem',
+      '& img': {
+        width: '27px',
+      },
+    },
+  },
+
 }));
 export default useStyles;
