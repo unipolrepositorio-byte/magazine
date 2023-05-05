58476485240884a2b44cc6b91a644dbf9ecc09ab
jesusm01 <jesusmoral011096@gmail.com>
2023-05-04 23:14:19 -0400

    menu volumes made just for volumes path

--- src/components/MenuComponent/index.jsx
+++ src/components/MenuComponent/index.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { SvgIcon } from '@material-ui/core';
-import { Grid, IconButton, Slide } from '@mui/material';
+import { Button, Grid, IconButton, Slide } from '@mui/material';
 import { useState, React } from 'react';
 import useStyles from './menuComponent.styles';
 import imageFooter from '../../assets/image/banner.jpg';
@@ -9,37 +9,63 @@ import ItemMenuDesktop from './itemMenuDesktop/index';
 import ItemMenuMobile from "./itemMenuMobile";
 import { ReactComponent as Hamburger } from '../../assets/image/hamburger.svg';
 import { ReactComponent as SearchIcon } from '../../assets/image/buscador.svg';
+import { useLocation } from 'react-router-dom';
 
 
 const MenuComponent = ({ items }) => {
     const classes = useStyles();
     const [open, setOpen] = useState(true);
+    const [volumeButton, setVolumeButton] = useState(false);
+    const volumes = [];
+    for(let i=0; i<=21; i++){
+        volumes.push(<Button variant='text'>{2023+i}</Button>)
+    }
+    const location = useLocation().pathname;
 
     return (
         <>
             <div className={classes.menuDesktop}>
-                <Grid container >
-                    <Grid item xs={2} />
-                    <Grid item xs container className={classes.menuContainer}>
-                        {items.map((item, index) => (
-                            <Link key={index} to={item.to} className={classes.link}>
-                                <ItemMenuDesktop label={item.name} variant='text' iconName={item.icon} />
-                            </Link>
-                        ))}
+                <Grid container direction='column'>
+                    <Grid item container>
+                        <Grid item xs={2} />
+                        <Grid item xs container className={classes.menuContainer}>
+                            {items.map((item, index) => (
+                                <Link key={index} to={item.to} className={classes.link}>
+                                    <ItemMenuDesktop label={item.name} variant='text' iconName={item.icon} />
+                                </Link>
+                            ))}
+                        </Grid>
+                        <Grid item xs={3} container className={classes.menuContainer}>
+                            {
+                            location==='/'&&
+                                <>
+                                <Link to={'/content/strapi'} className={classes.link}>
+                                    <ItemMenuDesktop label='INICIAR SESION' variant={'outlined'} />
+                                </Link>
+                                <Link to={'/register'} className={classes.link}>
+                                    <ItemMenuDesktop label='REGISTRARSE' variant={'contained'} />
+                                </Link>
+                                </>
+                            }
+                        </Grid>
+                        <Grid item xs={1} container className={classes.menuContainer}>
+                            <IconButton>
+                                <SearchIcon className={classes.icon} />
+                            </IconButton>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={3} container className={classes.menuContainer}>
-                        <Link to={'/content/strapi'} className={classes.link}>
-                            <ItemMenuDesktop label='INICIAR SESION' variant={'outlined'} />
-                        </Link>
-                        <Link to={'/register'} className={classes.link}>
-                            <ItemMenuDesktop label='REGISTRARSE' variant={'contained'} />
-                        </Link>
-                    </Grid>
-                    <Grid item xs={1} container className={classes.menuContainer}>
-                        <IconButton>
-                            <SearchIcon className={classes.icon} />
-                        </IconButton>
-                    </Grid>
+                    {location==='/volumes'&&
+                        <Grid item container direction='column' alignItems='center' className={classes.buttonVolumes} rowGap={2}>
+                            <Button variant='contained' onClick={()=>{setVolumeButton(prev => !prev)}}>{volumeButton?'BOTON':'AÑO'}</Button>
+                            {volumeButton&&<Grid container rowGap={1}>
+                                {volumes.map(item=>{return(
+                                    <Grid item >
+                                        {item}
+                                    </Grid>
+                                )})}
+                            </Grid>}
+                        </Grid>
+                    }
                 </Grid>
             </div>
 
@@ -54,12 +80,29 @@ const MenuComponent = ({ items }) => {
                     </Grid>
                 </Grid>
                 <Grid container className={classes.menuLoginMobile}>
-                    <Link to={'/content/strapi'} className={classes.link}>
-                        <ItemMenuDesktop label='INICIAR SESION' variant={'outlined'} />
-                    </Link>
-                    <Link to={'/register'} className={classes.link}>
-                        <ItemMenuDesktop label='REGISTRARSE' variant={'contained'} />
-                    </Link>
+                    {location==='/'&&<>
+                        <Grid container justifyContent='space-evenly' alignItems='center'>
+                            <Link to={'/content/strapi'} className={classes.link}>
+                                <ItemMenuDesktop label='INICIAR SESION' variant={'outlined'} />
+                            </Link>
+                            <Link to={'/register'} className={classes.link}>
+                                <ItemMenuDesktop label='REGISTRARSE' variant={'contained'} />
+                            </Link>
+                        </Grid>
+                    </>}
+                    {location==='/volumes'&&<>
+                        <Grid container direction='column' alignItems='flex-start' className={classes.buttonVolumes} rowGap={2}>
+                            <Button variant='contained' onClick={()=>{setVolumeButton(prev => !prev)}}>{volumeButton?'BOTON':'AÑO'}</Button>
+                            {volumeButton&&<Grid container columns={{xs:14}} rowGap={1} justifyContent='space-around'>
+                                {volumes.map(item=>{return(
+                                    <Grid item xs>
+                                        {item}
+                                    </Grid>
+                                )})}
+                            </Grid>}
+                        </Grid>
+                    </>}
+                    
                 </Grid>
                 <Slide direction="up" in={open} timeout={500}>
                     <div className={classes.containerMenuClosed}>
